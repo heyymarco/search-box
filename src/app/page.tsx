3f7e9757733d4f23be268b34538fc896452958a6
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-12-15 23:18:21 +0700

    updated SearchBox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,54 @@
 
 import { useState } from 'react'
 
+// reusable-ui core:
+import {
+    // react helper hooks:
+    useEvent,
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
+
 import styles from './page.module.css'
-import { SearchBox } from '@/components/SearchBox'
+import { SearchBox, SearchBoxSubmitEventHandler } from '@/components/SearchBox'
 
 
 
 export default function Home() {
+    const handleSubmit = useEvent<SearchBoxSubmitEventHandler>(async ({search, option}): Promise<void> => {
+        await new Promise<void>((resolved) => {
+            setTimeout(() => {
+                resolved();
+            }, 1000);
+        });
+        
+        
+        
+        console.log({search, option});
+    });
+    
+    
+    
+    // handlers:
     return (
         <main className={styles.main}>
-            <SearchBox theme='primary' />
+            <SearchBox
+                // configs:
+                options={[
+                    'Banana',
+                    'Apple',
+                    'Pear',
+                    'Orange',
+                ]}
+                
+                
+                
+                // variants:
+                theme='primary'
+                
+                
+                
+                // handlers:
+                onSubmit={handleSubmit}
+            />
         </main>
     )
 }
--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -17,7 +17,11 @@ import {
 import {
     // react helper hooks:
     useEvent,
-    EventHandler,
+    
+    
+    
+    // an accessibility management system:
+    AccessibilityProvider,
     
     
     
@@ -27,20 +31,7 @@ import {
 
 // reusable-ui components:
 import {
-    // base-components:
-    Generic,
-    Basic,
-    
-    
-    
-    // base-content-components:
-    Content,
-    
-    
-    
     // simple-components:
-    IconProps,
-    Icon,
     ButtonIcon,
     Input,
     
@@ -48,84 +39,190 @@ import {
     
     // layout-components:
     ListItem,
-    List,
-    
-    
-    
-    // status-components:
-    Badge,
-    Busy,
-    
-    
-    
-    // notification-components:
-    Alert,
     
     
     
     // menu-components:
-    Collapse,
     DropdownListButton,
-    
-    
-    
-    // dialog-components:
-    ModalExpandedChangeEvent,
     
     
     
     // composite-components:
     GroupProps,
     Group,
-    TabPanel,
 }                           from '@reusable-ui/components'          // a set of official Reusable-UI components
 
+// internals:
+import type {
+    SearchBoxSubmitEventHandler,
+}                           from './types'
 
 
+
+// handlers:
+const handleCancelFormSubmit : React.FormEventHandler<HTMLElement> = (event) => {
+    event.preventDefault();
+};
+
+
+
+// react components:
 export interface SearchBoxProps
     extends
         // bases:
-        GroupProps
+        Omit<GroupProps,
+            // handlers:
+            |'onSubmit' // overriden
+        >
 {
+    // configs:
+    options       ?: string[]
+    defaultOption ?: string
+    
+    
+    
+    // handlers:
+    onSubmit      ?: SearchBoxSubmitEventHandler
 }
 export const SearchBox = (props: SearchBoxProps): JSX.Element|null => {
+    // rest props:
+    const {
+        // configs:
+        options       = [],
+        defaultOption = '',
+        
+        
+        
+        // handlers:
+        onSubmit,
+    ...restGroupProps} = props;
+    
+    
+    
     // basic variant props:
     const basicVariantProps = useBasicVariantProps(props);
     
     
     
+    // states:
+    const [isBusy, setIsBusy] = useState<boolean>(false);
+    const [search, setSearch] = useState<string>('');
+    const [option, setOption] = useState<string>(defaultOption);
+    
+    
+    
+    // handlers:
+    const handleSearch = useEvent(async (): Promise<void> => {
+        setIsBusy(true);
+        try {
+            await onSubmit?.({
+                search,
+                option,
+            });
+        }
+        finally {
+            setIsBusy(false);
+        } // try
+    });
+    
+    
+    
+    // effects:
+    
+    // restore focus after search completed:
+    const inputRef = useRef<HTMLInputElement|null>(null);
+    useEffect(() => {
+        // conditions:
+        if (isBusy) return; // ignore if busy
+        
+        
+        
+        // actions:
+        inputRef.current?.focus();
+    }, [isBusy]);
+    
+    
+    
     // jsx:
     return (
-        <Group
-            // other props:
-            {...props}
+        <AccessibilityProvider
+            // states:
+            enabled={!isBusy}
         >
-            <Input type='search' required={true} />
-            <ButtonIcon
-                // appearances:
-                icon='search'
+            <Group
+                // other props:
+                {...restGroupProps}
+                
+                
+                
+                // semantics:
+                tag='form'
+                
+                
+                
+                // handlers:
+                onSubmit={handleCancelFormSubmit}
             >
-                Search
-            </ButtonIcon>
-            <DropdownListButton
-                // variants:
-                {...basicVariantProps}
-                
-                
-                
-                // floatable:
-                floatingPlacement='bottom-end'
-            >
-                <ListItem>
-                    Happy
-                </ListItem>
-                <ListItem>
-                    Sad
-                </ListItem>
-                <ListItem>
-                    Spooky
-                </ListItem>
-            </DropdownListButton>
-        </Group>
+                <Input
+                    // refs:
+                    elmRef={inputRef}
+                    
+                    
+                    
+                    // values:
+                    value={search}
+                    onChange={({target: {value}}) => setSearch(value)}
+                    
+                    
+                    
+                    // formats:
+                    type='search'
+                />
+                <ButtonIcon
+                    // appearances:
+                    icon='search'
+                    
+                    
+                    
+                    // behaviors:
+                    type='submit'
+                    
+                    
+                    
+                    // handlers:
+                    onClick={handleSearch}
+                >
+                    Search
+                </ButtonIcon>
+                {!!options.length && <DropdownListButton
+                    // variants:
+                    {...basicVariantProps}
+                    
+                    
+                    
+                    // floatable:
+                    floatingPlacement='bottom-end'
+                >
+                    {options.map((opt, index) =>
+                        <ListItem
+                            // identifiers:
+                            key={index}
+                            
+                            
+                            
+                            // states:
+                            active={option === opt}
+                            
+                            
+                            
+                            // handlers:
+                            onClick={() => setOption(opt)}
+                        >
+                            {opt}
+                        </ListItem>
+                    )}
+                </DropdownListButton>}
+            </Group>
+        </AccessibilityProvider>
     );
 }
